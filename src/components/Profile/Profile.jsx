a60602c54173b03ffde7441ddb1d58d4fca1e7c9
Denis070987 <deniskoms@gmail.com>
2023-01-05 11:14:08 +0200

    make profile section

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,34 +1,43 @@
 import PropTypes from 'prop-types'
 import css from './Profile.module.css'
 
-export const Profile = (username, tag, location, avatar, stats) => {
+export const Profile = ({username, tag, location, avatar, stats}) => {
     return (
         <div className={css.profile}>
-  <div class="description">
+  <div className={css.description}>
     <img
       src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
       alt="User avatar"
-      class="avatar"
+      className={css.avatar}
     />
-    <p class="name">Petra Marica</p>
-    <p class="tag">@pmarica</p>
-    <p class="location">Salvador, Brasil</p>
+    <p className="name">Petra Marica</p>
+    <p className="tag">@pmarica</p>
+    <p className="location">Salvador, Brasil</p>
   </div>
 
-  <ul class="stats">
+  <ul className="stats">
     <li>
-      <span class="label">Followers</span>
-      <span class="quantity">1000</span>
+      <span className="label">Followers</span>
+      <span className="quantity">1000</span>
     </li>
     <li>
-      <span class="label">Views</span>
-      <span class="quantity">2000</span>
+      <span className="label">Views</span>
+      <span className="quantity">2000</span>
     </li>
     <li>
-      <span class="label">Likes</span>
-      <span class="quantity">3000</span>
+      <span className="label">Likes</span>
+      <span className="quantity">3000</span>
     </li>
   </ul>
 </div>
     )
 }
+
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired
+};
