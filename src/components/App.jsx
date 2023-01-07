f519110aecf3cdea7aec016b4262f9e1e44571a3
Denis Boyko <deniskoms@gmail.com>
2023-01-07 14:43:10 +0200

    make friends section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
 
+import friends from 'data/friends.json'
 import user from 'data/user.json';
 import data from 'data/data.json'
 
@@ -14,6 +16,7 @@ export const App = () => {
   avatar={user.avatar}
         stats={user.stats} />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,17 @@
+import propTypes from 'prop-types';
+import css from './FriendList.module.css'
+
+export const FriendList = ({ friends }) =>
+{
+    return (
+        <div className={css.friendWrapper}>
+       <ul class="friend-list">
+  <li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li>
+</ul> 
+</div>
+    )
+}
