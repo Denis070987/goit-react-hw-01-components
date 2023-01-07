7838903e0e52c50be0d4acfc8775ffc94b15a6a5
Denis Boyko <deniskoms@gmail.com>
2023-01-07 14:25:27 +0200

    make Statistics section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
 
-import user from 'data/user.json'
+import user from 'data/user.json';
+import data from 'data/data.json'
 
 export const App = () => {
   return (
@@ -10,7 +12,8 @@ export const App = () => {
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
-  stats={user.stats}/>
+        stats={user.stats} />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,35 @@
+import propTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+function randColor() {
+    var r = Math.floor(Math.random() * (256)),
+        g = Math.floor(Math.random() * (256)),
+        b = Math.floor(Math.random() * (256));
+    return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+}
+
+export const Statistics = ({ title, stats }) => {return (
+    <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
+
+        <ul className={css.stat__list}>
+            {stats.map(({ id, label, percentage }) => {
+                return (
+        <li className={css.item}
+            key={id}
+            style={{backgroundColor: randColor()}}>
+                        <span className={css.label}>{label }</span>
+                        <span className={css.percentage}>{ percentage}</span>
+    </li>
+                )
+            })}
+    
+    </ul>
+</section>
+)
+}
+
+Statistics.propTypes = {
+    label: propTypes.string.isRequired,
+    percentage: propTypes.number.isRequired
+}
