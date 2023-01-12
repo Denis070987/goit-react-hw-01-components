91e99f5fd70374fb8422c24d172e581de74d94b0
Denis070987 <deniskoms@gmail.com>
2023-01-12 13:36:10 +0200

    fix bugs

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
 
-export const FriendListItem = ({ avatar, name, isOnline, id }) => {
+export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
       <span className={`${css.status} ${css[isOnline]}`}></span>
